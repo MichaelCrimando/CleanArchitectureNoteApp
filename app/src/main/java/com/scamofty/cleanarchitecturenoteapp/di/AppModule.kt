8c4cf20b57f1d27c89db86a26e84b71c7889bb84
Michael Crimando <crimandomichael@yahoo.com>
2023-05-31 23:18:51 -0400

    ViewModel done, next need to do notes screen

--- app/src/main/java/com/scamofty/cleanarchitecturenoteapp/di/AppModule.kt
+++ app/src/main/java/com/scamofty/cleanarchitecturenoteapp/di/AppModule.kt
@@ -6,13 +6,13 @@ import androidx.room.Room
 import com.scamofty.cleanarchitecturenoteapp.feature_note.data.data_source.NoteDatabase
 import com.scamofty.cleanarchitecturenoteapp.feature_note.data.repository.NoteRepositoryImpl
 import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.use_case.AddNote
 import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.use_case.DeleteNote
 import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.use_case.GetNotes
 import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -40,9 +40,8 @@ object AppModule {
     fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
         return NoteUseCases(
             getNotes = GetNotes(repository),
-            deleteNote = DeleteNote(
-                repository
-            )
+            deleteNote = DeleteNote(repository),
+            addNote = AddNote(repository)
         )
     }
 }
---
+++ app/src/main/java/com/scamofty/cleanarchitecturenoteapp/feature_note/domain/use_case/AddNote.kt
@@ -0,0 +1,22 @@
+package com.scamofty.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.model.InvalidNoteException
+import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.scamofty.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class AddNote (
+    private val repository: NoteRepository
+) {
+
+    @Throws(InvalidNoteException::class)
+    suspend operator fun invoke(note: Note) {
+        if(note.title.isBlank()){
+            throw InvalidNoteException("The title of the note can't be empty.")
+        } else if(note.content.isBlank()){
+            throw InvalidNoteException("The content of the note can't be empty.")
+        } else {
+            repository.insertNote(note)
+        }
+    }
+
+}
